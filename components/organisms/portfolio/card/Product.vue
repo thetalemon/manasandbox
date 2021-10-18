53266d2846302f0220938b225b200158269f9723
thetalemon <sasami0314@gmail.com>
2021-10-19 00:31:23 +0900

    feat: renewal portfolio card template

--- components/organisms/portfolio/card/Product.vue
+++ components/organisms/portfolio/card/Product.vue
@@ -1,24 +1,5 @@
 <template>
   <div class="card">
-    <h3>
-      {{ item.name }}
-      <span class="iconsWrapper">
-        <img
-          class="img"
-          :src="convert2Path('tech-logo', item.skill[0])"
-          width="20px"
-          height="20px"
-        >
-        <a :href="item.github" target="_blank">
-          <img
-            class="img"
-            :src="convert2Path('social', 'github.svg')"
-            width="20px"
-            height="20px"
-          >
-        </a>
-      </span>
-    </h3>
     <div class="imgWrapper">
       <a :href="item.url" target="_blank">
         <img
@@ -28,6 +9,24 @@
           height="150px"
         >
       </a>
+    </div>
+    <div class="textArea">
+      <h3>
+        {{ item.name }}
+        <span class="githubIcon">
+          <a :href="item.github" target="_blank">
+            <img
+              class="img"
+              :src="convert2Path('social', 'github.svg')"
+              width="20px"
+              height="20px"
+            >
+          </a>
+        </span>
+      </h3>
+      <p>
+        {{ item.text }}
+      </p>
     </div>
   </div>
 </template>
@@ -53,27 +52,30 @@ export default {
 <style lang="scss" scoped>
 .card {
   height: auto;
-  border: 2px solid #add8e6;
+  border: 1px solid #add8e6;
   border-radius: 4px;
-  padding: 4px;
+
+.textArea {
+    margin: 0 8px;
+  }
   h3 {
     display: inline-flex;
+  }
 
-    .iconsWrapper{
-      line-height: 25px;
-      padding-top: 2px;
-      margin-left: 8px;
-    }
+  .githubIcon{
+    line-height: 25px;
+    padding-top: 2px;
+    margin-left: 8px;
   }
 
   .imgWrapper{
     text-align: center;
     .img {
-      margin-top: 8px;
-      object-fit: contain;
-      max-width: 250px;
-      width: auto;
-      max-height: 150px;
+      margin-bottom: 8px;
+      border-radius: 4px 4px 0 0;
+      object-fit: cover;
+      width: 100%;
+      height: auto;
     }
   }
 }
--- mixins/ProductsMixin.js
+++ mixins/ProductsMixin.js
@@ -4,21 +4,21 @@ const _products = [
     img: 'portfolio.png',
     url: 'https://manasas.dev/',
     github: 'https://github.com/thetalemon/manasandbox',
-    skill: ['vuejs.svg']
+    text: 'このサイト。Nuxt.js製。'
   },
   {
     name: 'ブログ',
     img: 'blog.png',
     url: 'https://blog.manasas.dev/',
     github: 'https://github.com/thetalemon/manasblog',
-    skill: ['react.svg']
+    text: 'Next.js + microCMS。'
   },
   {
     name: 'あいきゃっちじぇねれーた',
     img: 'eyecatch.png',
     url: 'https://eyecatch.manasas.dev/',
     github: 'https://github.com/thetalemon/eye-catch-creator',
-    skill: ['react.svg']
+    text: '記事投稿する時のアイキャッチ作るやつ。Next.js + HTML5のcanvas機能。'
   }
 ]
 
