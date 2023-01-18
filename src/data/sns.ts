79a2c544002655b64ddb2fea17074571fa74dfae
thetalemon <sasami0314@gmail.com>
2023-01-18 23:21:51 +0900

    content: add instagram

--- src/data/sns.ts
+++ src/data/sns.ts
@@ -17,6 +17,14 @@ export const SNS_LIST:SnsItem[] = [
     url: 'https://stand.fm/channels/5f73270ff04555115d345d57',
   },
   {
+    name: 'Intagram(写真/日常・旅)',
+    url: 'https://www.instagram.com/manasas.photo/',
+  },
+  {
+    name: 'Intagram(イラスト)',
+    url: 'https://www.instagram.com/manasas.draw/',
+  },
+  {
     name: 'ブログ（雑記用）',
     url: 'https://manasas.hatenablog.jp/',
   },
