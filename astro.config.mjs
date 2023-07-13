f0e581de3c6f8d455ea9962e1c3d02c72d9010a0
thetalemon <sasami0314@gmail.com>
2023-07-13 23:38:08 +0900

    tailwind導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), tailwind()]
+  integrations: [image(), tailwind()],
 });
