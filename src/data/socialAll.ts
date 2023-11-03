2b6645d95dabdadc3a0c2a34856b3402c35e0e2f
thetalemon <sasami0314@gmail.com>
2023-11-03 11:39:57 +0900

    トップ画像変更

--- src/data/socialAll.ts
+++ src/data/socialAll.ts
@@ -10,7 +10,7 @@ export const SNS_LIST: SocialItem[] = [
     title: "fedibird(Mastodon)",
     url: "https://fedibird.com/@thetalemon",
     icon: "/social/mastodon.svg",
-    comment: "1番いる日常アカウント",
+    comment: "だいたいここにいる。雑多アカウント。",
   },
   {
     title: "Threads",
@@ -19,23 +19,16 @@ export const SNS_LIST: SocialItem[] = [
     comment: "ご飯と空ばかり上げてる写真アカウント",
   },
   {
-    title: "CoffeeBreak(Firefish)",
+    title: "CoffeeBreak(別館)",
     url: "https://annex.coffee-break.cc/@thetalemon",
     icon: "/social/firefish.ico",
-    comment: "映画の感想や短編を呟いている時もある",
-  },
-  {
-    title: "sealdolphin(めいどるふぃん・自鯖）",
-    url: "https://sealdolphin.manasas.dev/@manasas",
-    icon: "/social/dolphin.svg",
-    comment: "Fediverse界の自鯖、たまにブログの更新通知をしたりする。",
+    comment: "日記を書いている時もある。",
   },
   {
     title: "Twitter",
     url: "https://twitter.com/thetalemon",
     icon: "/social/twitter.svg",
-    comment:
-      "あまり見てない。エンジニア向け。たまにエンジニアっぽい発言をする。",
+    comment: "あまり見てない。エンジニアアカウント。",
   },
   {
     title: "Bluesky",
