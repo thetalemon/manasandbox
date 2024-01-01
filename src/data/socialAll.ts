adef2eaf350e7b3523f94e7b0a55bba2c98e5687
thetalemon <sasami0314@gmail.com>
2024-01-01 16:43:39 +0900

    TOPやSNSリストの整理

--- src/data/socialAll.ts
+++ src/data/socialAll.ts
@@ -2,7 +2,7 @@ type SocialItem = {
   url: string;
   title: string;
   icon: string;
-  comment: string;
+  comment?: string;
 };
 
 export const SNS_LIST: SocialItem[] = [
@@ -11,6 +11,18 @@ export const SNS_LIST: SocialItem[] = [
     url: "https://fedibird.com/@thetalemon",
     icon: "/social/mastodon.svg",
     comment: "だいたいここにいる。雑多アカウント。",
+  },
+  {
+    title: "Instagram",
+    url: "https://www.instagram.com/manasas.photo",
+    icon: "/social/instagram.svg",
+    comment: "そろそろ動かしたい",
+  },
+  {
+    title: "Twitter",
+    url: "https://twitter.com/thetalemon",
+    icon: "/social/twitter.svg",
+    comment: "あまり見てない。エンジニアアカウント",
   },
   {
     title: "Threads",
@@ -43,18 +55,6 @@ export const SNS_LIST: SocialItem[] = [
     comment: "過去の写真とか",
   },
   {
-    title: "Instagram",
-    url: "https://www.instagram.com/manasas.photo",
-    icon: "/social/instagram.svg",
-    comment: "そろそろ動かしたい",
-  },
-  {
-    title: "Twitter",
-    url: "https://twitter.com/thetalemon",
-    icon: "/social/twitter.svg",
-    comment: "あまり見てない。エンジニアアカウント",
-  },
-  {
     title: "Bluesky",
     url: "https://bsky.app/profile/manasas.dev",
     icon: "/social/bluesky.png",
@@ -64,12 +64,11 @@ export const SNS_LIST: SocialItem[] = [
     title: "ほしいものリスト",
     url: "https://www.amazon.jp/hz/wishlist/ls/O9U3OZI8D1VC?ref_=wl_sharen",
     icon: "/social/want.svg",
-    comment: "ほしいものリストです",
   },
   {
     title: "拍手（wavebox）",
     url: "https://wavebox.me/wave/1zpw4z8irp585mvq/",
     icon: "/social/clap.svg",
-    comment: "WEB拍手代わりにぜひ",
+    comment: "WEB拍手代わりです",
   },
 ];
