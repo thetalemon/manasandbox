0258597708f623ba7f96caaf297c3eda6f19854a
thetalemon <sasami0314@gmail.com>
2025-02-17 04:37:53 +0900

    同盟をsandboxに移動

---
+++ src/constants/constants.ts
@@ -0,0 +1,7 @@
+export const myDomain = 'manasas.dev'
+export const myUrl = `https://${myDomain}`
+export const myMainUrl = `https://manasas.dev`
+export const myName = 'manasas'
+export const mySiteName = 'manasandbox'
+export const mySiteDefaultDescription = 'サイト色々'
+export const myCopyLight = `© 2022- ${myName}`
