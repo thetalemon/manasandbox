b3e9844184c16c27a2aa136d74b576b1cc1e49f4
thetalemon <sasami0314@gmail.com>
2026-01-02 20:51:07 +0900

    RSSを整理

--- src/data/rssList.ts
+++ src/data/rssList.ts
@@ -1,41 +1,23 @@
 export type RssTargetItem = {
   url: string
   title: string
-  icon: string
+  link: string
 }
 
-export const RSS_LIST: RssTargetItem[] = [
-  {
-    title: '開発日誌',
-    url: 'https://journal.manasas.dev/feed',
-    icon: '/social/books.svg',
-  },
-  {
-    title: 'そらとぶアザラシ（雑記）',
-    url: 'https://blog.manasas.dev/feed',
-    icon: '/social/books.svg',
-  },
-  {
-    title: 'note',
-    url: 'https://note.com/thetalemon/rss',
-    icon: '/social/books.svg',
-  },
-  {
-    title: '読書・映画感想ブログ',
-    url: 'https://read.manasas.dev/rss',
-    icon: '/social/books.svg',
-  },
-  {
-    title: 'Zenn',
-    url: 'https://zenn.dev/thetalemon/feed',
-    icon: '/social/books.svg',
-  },
-]
+export const NOTE: RssTargetItem = {
+  title: 'note',
+  url: 'https://note.com/thetalemon/rss',
+  link: 'https://note.com/thetalemon',
+}
 
-export const DIARY_LIST: RssTargetItem[] = [
-  {
-    title: 'まなさす 日記',
-    url: 'https://day.manasas.dev/feed',
-    icon: '/social/books.svg',
-  },
-]
+export const BLOG: RssTargetItem = {
+  title: 'そらとぶアザラシ（雑記）',
+  url: 'https://blog.manasas.dev/feed',
+  link: 'https://blog.manasas.dev',
+}
+
+export const DIARY_LIST: RssTargetItem = {
+  title: 'まなさす 日記',
+  url: 'https://day.manasas.dev/feed',
+  link: 'https://day.manasas.dev',
+}
